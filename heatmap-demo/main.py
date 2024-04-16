4091b6dba0b035e64ee96a9a02f64093f1c74b00
Tomas Neubauer <tomas@quix.ai>
2024-04-16 19:31:33 +0100

    base

--- heatmap-demo/main.py
+++ heatmap-demo/main.py
@@ -1,24 +1,24 @@
 import os
 from quixstreams import Application
+import uuid
+import json
+from datetime import timedelta
 
 # for local dev, load env vars from a .env file
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Application.Quix("transformation-v1", auto_offset_reset="earliest")
+app = Application(consumer_group=str(uuid.uuid4()), auto_offset_reset="earliest", use_changelog_topics=False)
 
 input_topic = app.topic(os.environ["input"])
-output_topic = app.topic(os.environ["output"])
+#output_topic = app.topic(os.environ["output"])
 
 sdf = app.dataframe(input_topic)
 
-# put transformation logic here
-# see docs for what you can do
-# https://quix.io/docs/get-started/quixtour/process-threshold.html
 
-sdf = sdf.update(lambda row: print(row))
+sdf = sdf.update(lambda row: print(json.dumps(row, indent=4)))
 
-sdf = sdf.to_topic(output_topic)
+#sdf = sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
     app.run(sdf)
